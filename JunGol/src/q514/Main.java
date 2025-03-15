bf222d8903edf97b2595ea6c65e50d0a03035be2
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:46:38 +0900

    q514

---
+++ JunGol/src/q514/Main.java
@@ -0,0 +1,15 @@
+package q514;
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+		System.out.printf("height = ");
+		int a = sc.nextInt();
+
+        System.out.printf("Your height is "+a+"cm.");
+	}
+}
+
+
