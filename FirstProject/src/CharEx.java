7475ba5ed1d2a83c066db291da394dfa51204bbe
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:13:11 +0900

    first

---
+++ FirstProject/src/CharEx.java
@@ -0,0 +1,22 @@
+
+public class CharEx {
+	public static void main(String[] args) {
+//		char c1 = 'A';
+//		char c2 = 65;
+//		
+//		System.out.println(c1);
+//		System.out.println(c2);
+		
+		int a=5;
+		int b=0;
+		
+		System.out.println(a);
+		b+=a;
+		System.out.println(b);
+		
+		b*=a;
+		System.out.println(b);
+		
+		}
+
+}
