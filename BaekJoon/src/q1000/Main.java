b2211d3d97246370050af91ec4566901f36e54f1
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 16:41:24 +0900

    commit

---
+++ BaekJoon/src/q1000/Main.java
@@ -0,0 +1,21 @@
+package q1000;
+
+import java.util.Scanner;
+
+
+//import IOException;
+
+public class Main {
+	public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+		
+
+		int a = sc.nextInt();
+      int b = sc.nextInt();
+
+      sc.close();
+      
+      System.out.println(a+b);
+  }
+}
+      
