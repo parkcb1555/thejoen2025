3e9314eae656fda5950b59b8b1872a139b51f506
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:21:00 +0900

    q511

---
+++ JunGol/src/q511/Main.java
@@ -0,0 +1,18 @@
+package q511;
+
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args){
+        int a = 55;
+        int b = 10;
+        System.out.println(a+" - "+b+" = "+(a-b));
+
+        a = 2008;
+        b = 1999;
+        System.out.printf(a+" - "+b+" = "+(a-b));
+	}
+}
+
+
